08df60b049b8e62ffeec33568bd13af7fd0b941d
rawan93 <rawan93@users.noreply.github.com>
2016-02-03 12:39:35 +0300

    job name and length communicated from here

--- mpj/src/runtime/daemon/Wrapper.java
+++ mpj/src/runtime/daemon/Wrapper.java
@@ -27,14 +27,13 @@
  THE USE OR OTHER DEALINGS IN THE SOFTWARE.
  */
 
-/*
+ /*
  * File         : Wrapper.java 
  * Author       : Aamir Shafi, Bryan Carpenter
  * Created      : Dec 12, 2004
  * Revision     : $Revision: 1.20 $
  * Updated      : Aug 27, 2014
  */
-
 package runtime.daemon;
 
 import java.util.*;
@@ -45,110 +44,106 @@ import java.lang.reflect.*;
 import java.net.DatagramSocket;
 import java.net.ServerSocket;
 
-
 public class Wrapper extends Thread {
 
-  String portInfo = null;
-  int processes = 0;
-  String className = null;
-  Class c = null;
-  String deviceName = null;
-  String rank = null;
-  String[] nargs = null;
-  String hostName = null;
-  String args[] = null;
-  
-  String serverName = null;
-  int serverPort = 0;
-  private String WRAPPER_INFO = null;
-//test
-public double end; 
-  public Wrapper(ThreadGroup group, String name) {
-    super(group, name);
-  }
+    String portInfo = null;
+    int processes = 0;
+    String className = null;
+    Class c = null;
+    String deviceName = null;
+    String rank = null;
+    String[] nargs = null;
+    String hostName = null;
+    String args[] = null;
 
-  /**
-   * Executes MPJ program in a new JVM. This method is invoked in main
-   * method of this class, which is started by MPJDaemon. This method 
-   * can start multiple threads in a JVM.
-   * 
-   * @param args
-   *          Arguments to this method. args[0] is portInfo 'String',
-   *          args[1] is number of processes, args[2] is deviceName, args[3] is
-   *          hostname of MPJRun.java server, args[4] is the port number of
-   *          MPJRun.java server, args[5] is rank, args[6] is className
-   */
-  public void execute(String args[]) throws Exception {
-    InetAddress localaddr = InetAddress.getLocalHost();
-    hostName = localaddr.getHostName();
+    String serverName = null;
+    int serverPort = 0;
+    private String WRAPPER_INFO = null;
 
-    deviceName = args[2];
-    /* incase of mxdev the location of mpjdev.conf file is passed
+    public Wrapper(ThreadGroup group, String name) {
+        super(group, name);
+    }
+
+    /**
+     * Executes MPJ program in a new JVM. This method is invoked in main method
+     * of this class, which is started by MPJDaemon. This method can start
+     * multiple threads in a JVM.
+     *
+     * @param args Arguments to this method. args[0] is portInfo 'String',
+     * args[1] is number of processes, args[2] is deviceName, args[3] is
+     * hostname of MPJRun.java server, args[4] is the port number of MPJRun.java
+     * server, args[5] is rank, args[6] is className
+     */
+    public void execute(String args[]) throws Exception {
+        InetAddress localaddr = InetAddress.getLocalHost();
+        hostName = localaddr.getHostName();
+
+        deviceName = args[2];
+        /* incase of mxdev the location of mpjdev.conf file is passed
      * else the complete conf file string is passed
      * spaces in conf file contents were replaced by new line char
      * by tau_java.
-     */
-    if(!deviceName.equals("mxdev")){
-      args[0]=args[0].replace('|',' ');
-    }
-    portInfo = args[0];
-    processes = (new Integer(args[1])).intValue();
-    serverName = args[3];
-    serverPort = Integer.parseInt(args[4]);
-    rank = args[5];
-    className = args[6];
+         */
+        if (!deviceName.equals("mxdev")) {
+            args[0] = args[0].replace('|', ' ');
+        }
+        portInfo = args[0];
+        processes = (new Integer(args[1])).intValue();
+        serverName = args[3];
+        serverPort = Integer.parseInt(args[4]);
+        rank = args[5];
+        className = args[6];
 
-   
-    nargs = new String[(args.length - 7)];
-    System.arraycopy(args, 7, nargs, 0, nargs.length);
+        nargs = new String[(args.length - 7)];
+        System.arraycopy(args, 7, nargs, 0, nargs.length);
 
-    c = Class.forName(className);
+        c = Class.forName(className);
 
+        try {
 
-    try {
-   	
-      System.out.println("Starting process <"+rank+"> on <"+hostName+">");
+            System.out.println("Starting process <" + rank + "> on <" + hostName + ">");
+            double startTime = (double) System.currentTimeMillis() / 1000;
+            String arvs[] = new String[nargs.length + 3];
 
-      String arvs[] = new String[nargs.length + 3];
+            arvs[0] = rank;
+            if (!deviceName.equals("mxdev")) {
+                arvs[1] = portInfo.concat(";#Server Name;" + serverName
+                        + ";#Server Port;" + serverPort);
+            } else {
+                arvs[1] = portInfo;
+            }
+            arvs[2] = deviceName;
 
-      arvs[0] = rank;
-      if(!deviceName.equals("mxdev")){
-        arvs[1] = portInfo.concat(";#Server Name;"+serverName+
-                                  ";#Server Port;"+serverPort);
-      }
-      else{
-        arvs[1] = portInfo; 
-      }
-        arvs[2] = deviceName;
-
-      for (int i = 0; i < nargs.length; i++) {
-	arvs[i + 3] = nargs[i];
-      }
-      Method m = c.getMethod("main", new Class[] { arvs.getClass() });
-      m.setAccessible(true);
-      int mods = m.getModifiers();
-      if (m.getReturnType() != void.class || !Modifier.isStatic(mods)
-	  || !Modifier.isPublic(mods)) {
-	throw new NoSuchMethodException("main");
-      }
-      m.invoke(null, new Object[] { arvs });
-      //test: end time!
-      end = (double)System.currentTimeMillis()/1000;
-      System.out.println("Stopping Process <"+rank+"> on <"+hostName+">");      
-      
-      //write to output and get at MPJRun (name and port value are explicit for trail reasons)
-      InetAddress head = InetAddress.getByName("Reemis-MacBook-Pro.local");
+            for (int i = 0; i < nargs.length; i++) {
+                arvs[i + 3] = nargs[i];
+            }
+            Method m = c.getMethod("main", new Class[]{arvs.getClass()});
+            m.setAccessible(true);
+            int mods = m.getModifiers();
+            if (m.getReturnType() != void.class || !Modifier.isStatic(mods)
+                    || !Modifier.isPublic(mods)) {
+                throw new NoSuchMethodException("main");
+            }
+            m.invoke(null, new Object[]{arvs});
+            //test: end time!
+            double endTime = (double) System.currentTimeMillis() / 1000;
+            System.out.println("Stopping Process <" + rank + "> on <" + hostName + ">");
+            //calculate length
+            double length = endTime - startTime;
+            System.out.println("Dilmun , " + hostName + " , " + String.format("%.4f", length));
+            //write to output and get at MPJRun (name and port value are explicit for trail reasons)
+            /*InetAddress head = InetAddress.getByName("Reemis-MacBook-Pro.local");
       Socket headSock = new Socket(head, 40002);
 
       PrintWriter out = null;
       //DataInputStream in = null;
       try{
       	 //in = new DataInputStream(headSock.getInputStream());
-      	 out = new PrintWriter(headSock.getOutputStream());
-      	 out.write("Got ");
-      	 out.write(hostName);
+      	// out = new PrintWriter(headSock.getOutputStream());
+      	out.write("Got ");
+      	out.write(hostName);
       	 out.write(""+end);
-      	 out.flush();
+      	 //out.flush();
       	 //output = new PrintStream(headSock.getOutputStream());
          //output.print("got");
          //output.close();
@@ -164,12 +159,11 @@ public double end;
 
          }
          catch (IOException e) {
-       System.out.println(e);
-       }
-       }
-      
-      
-      /*
+          System.out.println(e);
+         }
+       }*/
+
+ /*
       if (headSock.isConnected()){
       	System.out.println("Connected");
       	try{
@@ -186,31 +180,28 @@ public double end;
        System.out.println(e);
        }
       }
-      */
-      //System.out.print(MPJRun.executedJob.get(hostName), "END = "+end) ;
-   		
+             */
+            //System.out.print(MPJRun.executedJob.get(hostName), "END = "+end) ;
+        } catch (Exception ioe) {
+            System.err.println("[" + hostName + "-Wrapper.java]: Multi-threaded"
+                    + " starter: exception" + ioe.getMessage());
+            ioe.printStackTrace();
+        }
     }
-    
-    catch (Exception ioe) {
-      System.err.println("["+hostName+"-Wrapper.java]: Multi-threaded"+
-                         " starter: exception" + ioe.getMessage());
-      ioe.printStackTrace();
-    }
-		}
 
-  public void run() {
-    try {
-      execute(args);     
+    public void run() {
+        try {
+            execute(args);
+        } catch (Exception ex) {
+            ex.printStackTrace();
+        }
     }
-    catch (Exception ex) {
-      ex.printStackTrace();
-    }
-  }
+
     public static void main(String args[]) throws Exception {
-    ThreadGroup group = new ThreadGroup("MPI" + args[3]);
-    Wrapper wrap = new Wrapper(group, args[3]);
-    wrap.args = args;
-    wrap.start();
-    wrap.join();
-     }
+        ThreadGroup group = new ThreadGroup("MPI" + args[3]);
+        Wrapper wrap = new Wrapper(group, args[3]);
+        wrap.args = args;
+        wrap.start();
+        wrap.join();
+    }
 }
